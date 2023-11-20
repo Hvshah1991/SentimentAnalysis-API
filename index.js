270d9612dc4cda9cb9968d515cd8797417f4a16b
Arvin Josol <arvin.josol@live.com>
2023-11-20 13:12:06 -0800

    add cors for communication with front end

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
 const { PORT, CORS_ORIGIN } = process.env
+
+app.use(cors({ origin: CORS_ORIGIN }))
+
+app.use(express.json())
 
 const inputRoutes = require("./routes/input")
 app.use("/input", inputRoutes)
