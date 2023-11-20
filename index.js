2ba82fcbda74cf5181005414920f1c0d02e4ff62
Harsh Shah <harshvshah.22@gmail.com>
2023-11-20 17:54:43 -0500

    new commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+//const quickstart = require('./routes/quickstart');
+
+require('dotenv').config();
+const { PORT } = process.env
+
+
+app.post('/', (_req, res) => {
+    res.send('Hello, your request is received')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+})
+
